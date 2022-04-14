abfd01911c1bc87ed98b3b83c0201f50ad230d2b
calimadoobe8 <101946367+calimadoobe8@users.noreply.github.com>
2022-04-14 16:33:18 +0300

    fddfd

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return view('hello world');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
